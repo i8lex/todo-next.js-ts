c71178ba4185ad7885eba8514e7a0304d5fc83df
i8lex <cybeerid@gmail.com>
2023-09-15 19:14:57 +0300

    fixed error with redux store and persist

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,48 +1,21 @@
-// import "@/styles/globals.css";
-// import React, { FC } from "react";
-// import type { AppProps } from "next/app";
-// import {persistor, wrapper} from "../redux/store";
-// import Header from "../components/Header";
-// import {PersistGate} from "redux-persist/integration/react";
-//
-// const WrappedApp: FC<AppProps> = ({ Component, pageProps }) => (
-//
-//     <PersistGate persistor={persistor} loading={null}>
-//         <>
-//             <Header />
-//             <Component {...pageProps} />
-//         </>
-//     </PersistGate>
-//
-// );
-// export default wrapper.withRedux(WrappedApp);
-
 import "@/styles/globals.css";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { createWrapper } from "next-redux-wrapper";
-import { store } from "@/redux/store";
-import { persistStore } from "redux-persist";
-import React, {FC} from "react";
+import { wrapper } from "@/redux/store";
+import React, { FC } from "react";
 import Header from "../components/Header";
-import {AppProps} from "next/app";
+import { AppProps } from "next/app";
 
-const App: FC<AppProps> = ({ Component, pageProps }) => {
-  const persistor = persistStore(store);
+const App: FC<AppProps> = ({ Component, pageProps, ...rest }) => {
+  const { store } = wrapper.useWrappedStore(rest);
 
   return (
     <Provider store={store}>
-      <PersistGate persistor={persistor} loading={null}>
-        <>
-          <Header />
-          <Component {...pageProps} />
-        </>
-      </PersistGate>
+      <>
+        <Header />
+        <Component {...pageProps} />
+      </>
     </Provider>
   );
 };
 
-const makeStore = () => store;
-const wrapper = createWrapper(makeStore);
-
-export default wrapper.withRedux(App);
+export default App;
