81b15668b4a428b0ea050dda73c0de854f3ba0ae
i8lex <cybeerid@gmail.com>
2023-09-29 00:15:51 +0300

    fixed for heroku

--- utils/socket.connection.ts
+++ utils/socket.connection.ts
@@ -1,11 +1,12 @@
 import { useEffect, useRef } from 'react';
 import { io, Socket } from 'socket.io-client';
+import * as process from 'process';
 
 const useSocket = (token: string, chatId?: string) => {
   const socketRef = useRef<Socket | null>(null);
 
   useEffect(() => {
-    socketRef.current = io('http://192.168.1.9:3001', {
+    socketRef.current = io(process.env.NEXT_PUBLIC_BASE_API_URL || 'URL', {
       query: { room: chatId },
       extraHeaders: {
         Authorization: `Bearer ${token}`,
