2b87a64fa688b804bea47e62ddae2d76624e419e
i8lex <cybeerid@gmail.com>
2023-09-28 23:22:41 +0300

    fixed my & users page, UserCard

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,4 +1,4 @@
-import { useGetUsersQuery, useLazyGetUsersQuery } from '@/redux/api/user.api';
+import { useGetUsersQuery } from '@/redux/api/user.api';
 import { GeneralLayout } from '@/components/layouts/General/Layout';
 import { UserCard } from '@/components/user/UserCard';
 import { Session } from 'next-auth';
@@ -34,20 +34,13 @@ const UsersPage = () => {
   const { data: users } = useGetUsersQuery(undefined, {
     refetchOnMountOrArgChange: true,
   });
-  const [getUsersTrigger] = useLazyGetUsersQuery();
 
   return (
     <>
       <GeneralLayout currentPage={'users'}>
         <div className="flex h-[89dvh] overflow-y-scroll flex-col gap-6 p-4 border border-stroke rounded-md bg-yellow-10 shadow-inner shadow-dark-60">
           {users?.map((user) => {
-            return (
-              <UserCard
-                lazyTrigger={getUsersTrigger}
-                key={user._id}
-                user={user}
-              />
-            );
+            return <UserCard key={user._id} user={user} />;
           })}
         </div>
       </GeneralLayout>
